e1c8ef5509b502bb152485653fb3d43e2b48584b
Brigitte Demers <guyoreo@gmail.com>
2020-10-24 10:28:17 -0400

    Finished 261: Added GameUI to GunEquipper

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Allows access to base Unity UI classes.
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    // The SerializeField attribute.Attributes allow you to convey information to the
+    // C# runtime about particular classes.In this case, you are declaring that variables
+    // are accessible from the Unity Inspector but not from other scripts.
+
+    [SerializeField]
+    Sprite redReticle;
+    [SerializeField]
+    Sprite yellowReticle;
+    [SerializeField]
+    Sprite blueReticle;
+    [SerializeField]
+    Image reticle;
+
+    public void UpdateReticle()
+    {
+        switch (GunEquipper.activeWeaponType)
+        {
+            case Constants.Pistol:
+                reticle.sprite = redReticle;
+                break;
+            case Constants.Shotgun:
+                reticle.sprite = yellowReticle;
+                break;
+            case Constants.AssaultRifle:
+                reticle.sprite = blueReticle;
+                break;
+            default:
+                return;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
