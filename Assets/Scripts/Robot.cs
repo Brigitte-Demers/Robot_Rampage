143c7517ef5d93dcff7ce66d0ed85d7f12edfade
Brigitte Demers <guyoreo@gmail.com>
2020-11-04 11:06:14 -0500

    Finished Page 272: Robot Rampage

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    // Robot type is the type of robot. The red, blue or yellow robot's
+    // to be specific.
+    [SerializeField]
+    private string robotType;
+
+    // How much damage this robot can take before being destroyed.
+    public int health;
+
+    // Distance their guns can fire.
+    public int range;
+
+    //How fast their guns can fire.
+    public float fireRate;
+
+    public Transform missileFireSpot;
+
+    // Agent is a reference to the NavMesh Agent Component.
+    UnityEngine.AI.NavMeshAgent agent;
+
+    // What the robot's should be tracking.
+    private Transform player;
+    private float timeLastFired;
+
+    // Tracks whether or not the robot is dead/destroyed.
+    private bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1.
+        // By default, all robots are alive. By setting the agent and player values
+        // to the NavMesh Agent and Player components respectively.
+        isDead = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 2. 
+        // Check if the robot is dead before continuing. There are no
+        // zombie robots in this game!
+        if (isDead)
+        {
+            return;
+        }
+
+        // 3.
+        // Makes the robot face the player.
+        transform.LookAt(player);
+
+        // 4.
+        // Tells the robot to use the NavMesh to find the player.
+        agent.SetDestination(player.position);
+
+        // 5.
+        // Checks to see if the robot is within firing range and if there's been enough
+        // time between shots to fire again.
+        if (Vector3.Distance(transform.position, player.position) < range
+        && Time.time - timeLastFired > fireRate)
+        {
+            // 6.
+            // Updates timeLastFired to the current time and calls Fire(), which simply logs a
+            // message to the console for the time being.
+            timeLastFired = Time.time;
+            fire();
+        }
+    }
+
+    private void fire()
+    {
+        // I need to include the 'UnityEngine' when using 'Debug'. It does not work unless
+        // I do it like that.
+        UnityEngine.Debug.Log("Fire");
+    }
+}
